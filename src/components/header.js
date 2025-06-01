4a04295032cd5490cd6d4109a387ce966c596302
Tim Friedrich <tim.friedrich@lichtblick.de>
2025-06-01 15:51:04 +0200

    feat: Enhance header interactivity by adding click functionality to logo and site title

--- src/components/header.js
+++ src/components/header.js
@@ -4,5 +4,5 @@ export function renderHeader(siteTitle) {
   document.title = siteTitle;
   const h1 = document.querySelector('header h1');
   console.log("base URL:", import.meta.env.BASE_URL);
-  h1.innerHTML = `<img src="${import.meta.env.BASE_URL}logo.svg" alt="Logo" class="logo"> ${siteTitle}`;
+  h1.innerHTML = `<img src="${import.meta.env.BASE_URL}logo.svg" alt="Logo" class="logo" onclick="window.location.href='https://olgas-kitchen.de'" style="cursor:pointer;"> <span onclick="window.location.href='https://olgas-kitchen.de'" style="cursor:pointer;">${siteTitle}</span>`;
 }
